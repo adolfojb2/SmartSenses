5fa0e827ccee0999dac86c0a6048da6072864b17
adolfojb2 <adolfojb2@gmail.com>
2024-07-01 13:07:24 -0500

    Configuración para usar postgresql en django, creación de la tabla users y primera migración.

--- SmartSenses/settings.py
+++ SmartSenses/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'SmartSenses.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'smartsenses', #nombre de la BD
+        'USER': 'postgres',
+        'PASSWORD': 'ajb230',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
--- cuentas/models.py
+++ cuentas/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class users(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.CharField(max_length=100, unique=True)
+    password = models.CharField(max_length=128)
+    phonenumber = models.CharField(max_length=15)
+    photo = models.CharField(max_length=150, blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
