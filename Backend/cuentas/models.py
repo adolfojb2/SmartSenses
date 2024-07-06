ae4c1104f8b580131e7968da1a1807429da9ec11
adolfojb2 <adolfojb2@gmail.com>
2024-07-06 10:00:57 -0500

    Configuración django-admin

--- Backend/cuentas/models.py
+++ Backend/cuentas/models.py
@@ -2,9 +2,12 @@ from django.db import models
 
 # Create your models here.
 
-class users(models.Model):
+class Users(models.Model):
     name = models.CharField(max_length=100)
     email = models.CharField(max_length=100, unique=True)
     password = models.CharField(max_length=128)
     phonenumber = models.CharField(max_length=15)
     created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
