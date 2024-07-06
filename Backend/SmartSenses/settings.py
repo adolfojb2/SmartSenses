6dd40387b7489cb46cf900af0eaf86f2632543c8
adolfojb2 <adolfojb2@gmail.com>
2024-07-06 08:49:22 -0500

    Configuración djangorestframework.

--- Backend/SmartSenses/settings.py
+++ Backend/SmartSenses/settings.py
@@ -37,12 +37,15 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
+    'rest_framework',
     'cuentas',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -126,3 +129,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#Servidores que se pueden conectar a Django (cors authorization)
+CORS_ALLOWED_ORIGINS = [
+    
+]
