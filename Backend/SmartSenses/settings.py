061389011cdeb79d717874d274812673a3b93f36
“adolfojb2” <adolfojb2@gmail.com>
2024-09-03 16:47:10 -0500

    Creación de la tabla divices en la DB

--- Backend/SmartSenses/settings.py
+++ Backend/SmartSenses/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'coreapi',
     'cuentas',
+    'dispositivos',
 ]
 
 MIDDLEWARE = [
--- Backend/dispositivos/models.py
+++ Backend/dispositivos/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from cuentas.models import users
 
-# Create your models here.
+class Device(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    location = models.CharField(max_length=255)
+    user_id = models.ForeignKey(users, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
